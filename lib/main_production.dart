d5b5fcc4e94a54823ec548483d361f3a70037708
Marvin <marvin@poopjournal.rocks>
2024-06-30 00:19:42 +0200

    Update Build Configuration

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,9 @@
 import 'package:poop_journal/app/app.dart';
 import 'package:poop_journal/bootstrap.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:poop_journal/firebase_options.dart';
 
 await Firebase.initializeApp(
-options: DefaultFirebaseOptions.currentPlatform,
+options = DefaultFirebaseOptions.currentPlatform,
 );
 f
 void main() {
