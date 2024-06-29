b11f62d0adafd94212e157340e0695dba96970c5
Marvin <marvin@poopjournal.rocks>
2024-06-29 23:44:46 +0200

    Add Firebase Plugins

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,11 @@
 import 'package:poop_journal/app/app.dart';
 import 'package:poop_journal/bootstrap.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
 
 void main() {
   bootstrap(() => const App());
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,12 @@
 import 'package:poop_journal/app/app.dart';
 import 'package:poop_journal/bootstrap.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
+f
 void main() {
   bootstrap(() => const App());
 }
